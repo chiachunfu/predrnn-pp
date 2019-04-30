a12ead9e27b8734b5a8e88e0d994bbd1c713fbe7
Chia-Chun Fu <chiachun.fu@gmail.com>
2019-04-30 09:57:03 -0700

    test

--- train.py
+++ train.py
@@ -142,6 +142,38 @@ class Model(object):
         self.saver.save(self.sess, checkpoint_path, global_step=itr)
         print('saved to ' + FLAGS.save_dir)
 
+
+class ImageCallback(Callback):
+    def on_epoch_end(self, epoch, logs):
+        validation_X, validation_y = next(
+            my_generator(15, val_dir))
+        output = self.model.predict(validation_X)
+        wandb.log({
+            "input": [wandb.Image(np.concatenate(np.split(c, 5, axis=2), axis=1)) for c in validation_X],
+            "output": [wandb.Image(np.concatenate([validation_y[i], o], axis=1)) for i, o in enumerate(output)]
+        }, commit=False)
+
+
+def my_generator(batch_size, img_dir):
+    """A generator that returns 5 images plus a result image"""
+    cat_dirs = glob.glob(img_dir + "/*")
+    counter = 0
+    while True:
+        input_images = np.zeros(
+            (batch_size, config.width, config.height, 3 * 5))
+        output_images = np.zeros((batch_size, config.width, config.height, 3))
+        random.shuffle(cat_dirs)
+        if ((counter+1)*batch_size >= len(cat_dirs)):
+            counter = 0
+        for i in range(batch_size):
+            input_imgs = glob.glob(cat_dirs[counter + i] + "/cat_[0-5]*")
+            imgs = [Image.open(img) for img in sorted(input_imgs)]
+            input_images[i] = np.concatenate(imgs, axis=2)
+            output_images[i] = np.array(Image.open(
+                cat_dirs[counter + i] + "/cat_result.jpg"))
+        yield (input_images, output_images)
+        counter += batch_size
+
 def main(argv=None):
     if tf.gfile.Exists(FLAGS.save_dir):
         tf.gfile.DeleteRecursively(FLAGS.save_dir)
